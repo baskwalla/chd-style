4998279ca2ffb45e88f45b7a6fa0963ba611bff9
baskwalla <baskwalla@gmail.com>
2013-04-07 20:08:49 -0500

    added jquery

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// ******** Off Canvas Layout ***********
+// **************************************
+
+$(document).ready(function() {
+  
+  $('body').addClass('js');
+  
+  var $menu = $('#menu'),
+    $menulink = $('.menu-link'),
+    $wrap = $('#wrap');
+  
+  $menulink.click(function() {
+    $menulink.toggleClass('active');
+    $wrap.toggleClass('active');
+    return false;
+  });
+});
